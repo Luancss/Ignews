a914eea7177da0beee1ab1ebf57c3597f13fb4dc
Luancss <ventobo123@gmail.com>
2023-08-30 21:23:14 -0300

    Componente - Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active} href="">Home</a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app'
-import './styles/global.scss';
+import '../styles/global.scss';
+import { Header } from '../components/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <>
+    <Header/>
+   <Component {...pageProps} />
+   </>
+  )
 }
 
 export default MyApp;
