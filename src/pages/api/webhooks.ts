02e3cdf9845c57691f1a97b75bb852b9f826e3af
Luancss <ventobo123@gmail.com>
2023-09-09 01:05:23 -0300

    Salvando dados do evento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream'
 import Stripe from "stripe";
 import { stripe } from '../../services/stripe'
+import { saveSubscription } from "./_lib/manageSubscription";
 
   async function buffer(readable: Readable) {
     const chunks = []
@@ -40,7 +41,24 @@ export default async (rec: NextApiRequest, res: NextApiResponse) => {
   const {type} = event;
 
   if (relevantEvents.has(type)) {
-console.log('Evento recebido', event);
+    try {
+    switch(type) {
+      case 'checkout.session.completed':
+
+      const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+        await saveSubscription(
+          checkoutSession.subscription.toString(),
+          checkoutSession.customer.toString()
+        )
+
+        break;
+      default:
+        throw new Error ('Unhandled event.')
+    }
+  } catch (err){
+    return res.json({error: 'Webhook handler failed'})
+  }
 
   }
 
