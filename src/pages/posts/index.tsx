8eb8b7dc65a48169455b54eb2f777c9b6f55c085
Luancss <ventobo123@gmail.com>
2023-09-10 21:14:09 -0300

    Página - Posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+export default function Posts()  {
+  return (
+    <>
+    <Head>
+      <title>Posts | Ignews</title>
+    </Head>
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        <a href=''>
+          <time>12 de abril de 2022</time>
+          <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+          <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+        </a>
+        <a href=''>
+          <time>12 de abril de 2022</time>
+          <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+          <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+        </a>
+        <a href=''>
+          <time>12 de abril de 2022</time>
+          <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+          <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+        </a>
+      </div>
+    </main>
+    </>
+  )
+}
