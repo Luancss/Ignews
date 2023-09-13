9378e277b64b3beaf1ee5ea93b2c63f320ee5c24
Luancss <ventobo123@gmail.com>
2023-09-13 01:57:01 -0300

    Página - Post

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,7 +8,7 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          <a href="/posts">Posts</a>
         </nav>
         <SignInButton/>
       </div>
